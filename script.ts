d62e27ff140533459d7fc04d167b4662856e1124
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-08-25 05:31:19 +0930

    [prisma-code] Using prisma in code for the first time. And also configured package.json and tsconfig.json to work with nodemon.

--- script.ts
+++ script.ts
@@ -1,4 +1,22 @@
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from './generated/prisma/index.js'
 
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Deepika'
+    }
+  })
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(() => {
+    // This is not actually necessary because Prisma disconnects itself after query finished running but still safe
+    prisma.$disconnect();
+  })
